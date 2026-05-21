fc94fa56406482b5c490a3ff0ad68c6189cabff4
the-letter-h0000 <210426248+the-letter-h0000@users.noreply.github.com>
2026-05-21 12:29:21 +0200

    Update _HttpUtilGetStatusCode.c
    it's just the response code... not the message too (e.g. 200 OK, etc)

--- func decomps/apps/_HttpUtilGetStatusCode.c
+++ func decomps/apps/_HttpUtilGetStatusCode.c
@@ -1,7 +1,7 @@
 int _HttpUtilGetStatusCode(char *rawResponseString)
 {
   int StatusCode;
-  undefined1 FullResponseStatusCode [16];
+  undefined1 charStatusCode [16];
   undefined1 HttpVersion [16];
   
   if (rawResponseString == (char *)0x0)
@@ -11,9 +11,9 @@ int _HttpUtilGetStatusCode(char *rawResponseString)
   else
   {
     AcMemsetEx(HttpVersion,0,0x10,0x10,DAT_81412cf4,0x250);
-    AcMemsetEx(FullResponseStatusCode,0,0x10,0x10,DAT_81412cf4,DAT_81412e18);
-    __0sscanf(rawResponseString,s_%s_%s_81412e1c,HttpVersion,FullResponseStatusCode);
-    StatusCode = AcAtoi(FullResponseStatusCode);
+    AcMemsetEx(charStatusCode,0,0x10,0x10,DAT_81412cf4,DAT_81412e18);
+    __0sscanf(rawResponseString,s_%s_%s_81412e1c,HttpVersion,charStatusCode);
+    StatusCode = AcAtoi(charStatusCode);
     SysDebugPrintf(DAT_81412d70,s__HttpUtilGetStatusCode:_Version:_81412e24,HttpVersion,StatusCode);
   }
   return StatusCode;
